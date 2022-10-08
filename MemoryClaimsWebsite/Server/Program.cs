d5b215c99739904b410ec7527a5cc11856b513ed
Pirasath <connect@object.social>
2022-10-08 22:34:39 +0200

    Remove the default Error page on server end.

--- MemoryClaimsWebsite/Server/Program.cs
+++ MemoryClaimsWebsite/Server/Program.cs
@@ -11,16 +11,9 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
     app.UseWebAssemblyDebugging();
-}
 else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
