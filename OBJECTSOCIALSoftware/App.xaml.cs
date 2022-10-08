79767faef9171cdbde2890faf8e8c9cce434cc73
Pirasath <connect@object.social>
2022-10-08 18:56:09 +0200

    Why do i use blazor. So i dont need to keep code the same User interface. Well i found a way to make Icon for MAUI project. My next goal is how to make it look like in windows and mac.

---
+++ OBJECTSOCIALSoftware/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OBJECTSOCIALSoftware
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ OBJECTSOCIALSoftware/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OBJECTSOCIALSoftware
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
