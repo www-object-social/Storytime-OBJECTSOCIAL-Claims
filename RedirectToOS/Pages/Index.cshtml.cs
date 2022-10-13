0f76fe06dfcab7fc870867e0a848fc4a5347358f
Pirasath <connect@object.social>
2022-10-13 14:12:29 +0200

    Update RedirectToOS

---
+++ RedirectToOS/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RedirectToOS.Pages
+{
+    public class IndexModel : PageModel
+    {
+       
+        public void OnGet()
+        {
+            Response.Redirect("https://object.social");
+        }
+    }
+}
