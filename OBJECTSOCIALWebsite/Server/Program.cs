bbc79312367c6dbe8a810cf5ac1bae6058f69190
Pirasath <connect@object.social>
2022-10-13 11:22:46 +0200

    now singalr dont want to start

--- OBJECTSOCIALWebsite/Server/Program.cs
+++ OBJECTSOCIALWebsite/Server/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-
 builder.Services.AddSignalR();
 builder.Services.AddResponseCompression(opts =>
 {
@@ -21,20 +20,12 @@ if (app.Environment.IsDevelopment())
     app.UseWebAssemblyDebugging();
 else
     app.UseHsts();
-app.UseCors(builder =>
-{
-    builder.SetIsOriginAllowedToAllowWildcardSubdomains()
-        .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
-});
 app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 app.MapHealthChecks("/os-and-claims-healthchecks");
 app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapHub<Backstage.Backstage>("/os-and-claims-backstage");
-});
+app.MapHub<Backstage.Backstage>("/os-and-claims-backstage");
 app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
